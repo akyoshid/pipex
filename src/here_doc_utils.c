4ff265230d12da6880d6bb5c130c4e0b0fde31b7
akyoshid <akyoshid@student.42tokyo.jp>
2025-01-12 18:30:53 +0000

    [RE-RFCT] some functions were separated from here_doc_utils.c to here_doc_limiter.c

--- src/here_doc_utils.c
+++ src/here_doc_utils.c
@@ -6,60 +6,11 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 11:48:22 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/01/10 17:00:15 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/01/12 17:42:33 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
-
-int	ft_rand_bytes(char *dst, int len)
-{
-	int	fd;
-	int	read_rv;
-
-	if (len <= 0)
-		return (RAND_ERR_INVALID_LEN);
-	fd = open("/dev/urandom", O_RDONLY);
-	if (fd == -1)
-		return (RAND_ERR_OPEN);
-	read_rv = read(fd, dst, len);
-	if (close(fd) == -1)
-		return (RAND_ERR_CLOSE);
-	if (read_rv == -1)
-		return (RAND_ERR_READ_FAIL);
-	else if (read_rv != len)
-		return (RAND_ERR_READ_LACK);
-	return (RAND_SUCCESS);
-}
-
-void	set_here_doc_path(t_data *data)
-{
-	char	rand[11];
-	int		rand_rv;
-	int		i;
-
-	ft_strlcpy(data->here_doc_path, "/tmp/sh-thd-", 23);
-	rand_rv = ft_rand_bytes(rand, 10);
-	if (rand_rv == RAND_ERR_OPEN)
-		exit_pipex(data, EXIT_FAILURE, ERR_OPEN, "/dev/urandom");
-	else if (rand_rv == RAND_ERR_READ_FAIL)
-		exit_pipex(data, EXIT_FAILURE, ERR_READ, NULL);
-	else if (rand_rv == RAND_ERR_READ_LACK)
-		exit_pipex(data, EXIT_FAILURE, ERR_PARAM, "pipex: "
-			"ft_rand_bytes: Failed to read the specified number of bytes");
-	else if (rand_rv == RAND_ERR_CLOSE)
-		exit_pipex(data, EXIT_FAILURE, ERR_CLOSE, NULL);
-	i = -1;
-	while (++i < 10)
-	{
-		rand[i] = rand[i] % 36;
-		if (rand[i] <= 9)
-			data->here_doc_path[i + 12] = rand[i] + '0';
-		else
-			data->here_doc_path[i + 12] = rand[i] - 10 + 'a';
-	}
-	data->here_doc_path[22] = '\0';
-}
 
 void	proc_gnl_err(t_data *data, int return_code)
 {
