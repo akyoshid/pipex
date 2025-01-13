1a778432d94e74f88b6108d8dac734b0529c2f72
akyoshid <akyoshid@student.42tokyo.jp>
2025-01-13 15:02:47 +0000

    [FEAT] here_doc_expand_var()

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,11 +6,86 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 11:48:44 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/01/12 18:48:49 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/01/13 15:01:09 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
+
+int	calc_var_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '$')
+	{
+		i = 1;
+		if (ft_isalpha(str[1]) == 1 || str[1] == '_')
+		{
+			i = 2;
+			while (ft_isalnum(str[i]) == 1 || str[i] == '_')
+				i++;
+		}
+	}
+	return (i);
+}
+
+// Find a `key` in `data->env_list` that matches `str` and return its `value`.
+// `str` must be null-terminated.
+char	*search_var_value(t_data *data, char *str)
+{
+	t_env *current_node;
+
+	current_node = data->env_list;
+	while (current_node != NULL)
+	{
+		if (ft_strcmp(current_node->key, str) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
+
+char	*here_doc_expand_var(t_data *data, char *src)
+{
+	int		i;
+	int		var_len;
+	char	temp;
+	char	*var_value;
+	char	*new;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		if (src[i] == '$')
+		{
+			var_len = calc_var_len(src + i);
+			temp = src[i + var_len];
+			src[i + var_len] = '\0';
+			var_value = search_var_value(data, src + i + 1);
+			src[i + var_len] = temp;
+			if (var_value == NULL)
+				ft_strlcpy(src + i, src + i + var_len, ft_strlen(src) - i + 1);
+			else
+			{
+				new = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1 - var_len + ft_strlen(var_value)));
+				if (new == NULL)
+				{
+					free(src);
+					exit_pipex(data, EXIT_FAILURE, ERR_MALLOC, NULL);
+				}
+				ft_strncpy(new, src, i);
+				ft_strncpy(new + i, var_value, ft_strlen(var_value));
+				ft_strlcpy(new + i + ft_strlen(var_value), src + i + var_len, (ft_strlen(src) + 1 - var_len - i));
+				free(src);
+				src = new;
+			}
+		}
+		else
+			i++;
+	}
+	return (src);
+}
 
 void	here_doc_delete_tab(char *new_line)
 {
@@ -44,8 +119,8 @@ bool	here_doc_success(t_data *data, char *new_line, t_heredoc *hd_data)
 	}
 	else
 	{
-		if (hd_data->leading_hyphen == true)
-			here_doc_delete_tab(new_line);
+		if (hd_data->quoted_limiter == false)
+			new_line = here_doc_expand_var(data, new_line);
 		if (write(data->in_fd, new_line, ft_strlen(new_line)) == -1)
 		{
 			free(new_line);
